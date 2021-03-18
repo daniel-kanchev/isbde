59beb3c06199a244493ff3f3932018e3808eff52
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-18 12:25:32 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,14 @@
+URL 1: https://isb.rlp.de/presse.html
+
+URL 2: https://isb.rlp.de/aktuelles/news.html
+    
+    Spider name: isbde
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
+- scraping from 2 sources, press and news
---
+++ isbde/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'isbde'
+SPIDER_MODULES = ['isbde.spiders']
+NEWSPIDER_MODULE = 'isbde.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'isbde.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
